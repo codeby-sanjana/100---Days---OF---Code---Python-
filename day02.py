3e2b0ab7cbf9274d6522afbbbf9a1074998b0556
codeby-sanjana <sanjanapawar2008@gmail.com>
2026-09-12 19:44:16 +0530

    Create day02.py

---
+++ day02.py
@@ -0,0 +1,8 @@
+# Day 2: Understanding Print and Basic Math
+# Following CodeWithHarry's 100 Days of Code
+
+# 1. Printing plain text sentences
+print("Hello! This is my Day 2 of learning Python.")
+print("Python is incredibly easy to read and write.")
+
+
