0b5685fb231de64e6a74128f8127a805079b76c9
codeby-sanjana <sanjanapawar2008@gmail.com>
2026-09-14 20:11:26 +0530

    Create day04.py

---
+++ day04.py
@@ -0,0 +1,14 @@
+# Day 4: Master Python Comments
+
+# 1. This is a single-line comment.
+# It explains the print statement right underneath it.
+print("Comments are completely hidden in the output!")
+
+print("Hello World")  # 2. This is an inline comment (written next to code)
+
+"""
+3. This is a multi-line comment using triple quotes.
+You can write paragraphs of text here across multiple lines
+without needing to type a '#' symbol at the start of each line!
+"""
+print("Python ignores everything inside the triple quotes!")
