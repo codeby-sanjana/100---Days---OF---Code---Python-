08a492a4db104a603eea1c947014e2e0f3f6efc7
codeby-sanjana <sanjanapawar2008@gmail.com>
2026-09-15 20:27:01 +0530

    Create day05.py

---
+++ day05.py
@@ -0,0 +1,17 @@
+# Day 5: More on Print Statements
+
+# 1. Python Code
+print("Hey I am a \"good girl\"\nand this viewer is also a good girl")
+print() 
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Sanjana")
+
+# ----------------------------------------
+# 🖥️ EXPECTED OUTPUT (Python ignores this block):
+# ----------------------------------------
+# Hey I am a "good boy"
+# and this viewer is also a good boy
+# 
+# Hey~6~7009
+# Sanjana 
+# ----------------------------------------
