7c89fea1dee9e2417e519a6c82c3c6f4c8bc4391
Sanjana Pawar <sanjanapawar2008@gmail.com>
2026-09-17 19:50:56 +0530

    Create day07.py

---
+++ day07.py
@@ -0,0 +1,12 @@
+# Day 7 : PYTHON OPERATORS 
+a = 15
+b = 2
+
+# Arithmetic Calculations
+print(a + b)   # Output: 17
+print(a - b)   # Output: 13
+print(a * b)   # Output: 30
+print(a / b)   # Output: 7.5
+print(a // b)  # Output: 7  (Drops the decimal part)
+print(a % b)   # Output: 1  (Returns the remainder)
+print(a ** b)  # Output: 225 (15 raised to power of 2)
