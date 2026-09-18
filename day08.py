54dcc178249dc5a3ebef83feaf838640f77946ad
Sanjana Pawar <sanjanapawar2008@gmail.com>
2026-09-18 20:12:14 +0530

    Create day08.py

---
+++ day08.py
@@ -0,0 +1,8 @@
+# Day 8: Exercise1 create a calculator 
+a = 60
+b = 5
+
+print("The value of", a, "+", 5, "is: ", a + b)
+print("The value of", a, "-", 5, "is: ", a - b)
+print("The value of", a, "*", 5, "is: ", a * b)
+print("The value of", a, "/", 5, "is: ", a / b)
