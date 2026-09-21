f768b8f49e097ab439fa8ee0e06e8a6acad3a83a
Sanjana Pawar <sanjanapawar2008@gmail.com>
2026-09-21 19:36:05 +0530

    Create day11.py

---
+++ day11.py
@@ -0,0 +1,29 @@
+# Day 11 - Strings in Python 
+
+name = "Sanjana"
+friend = "Priya"       
+anotherFriend = 'Neha'
+
+# 1. Defining a Multiline String
+apple = '''He said, 
+Hi Sanjana
+I am good
+"I want to eat an apple'''
+
+# Printing strings
+print("Hello, " + name)
+print(apple)
+
+# 2. Accessing Characters by Index
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6]) 
+
+# 3. Looping Through a String
+print("\nLets use a for loop\n")
+for character in apple:
+    print(character)
